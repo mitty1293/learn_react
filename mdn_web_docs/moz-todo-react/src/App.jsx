acfc946c7fed3a53ffd4d4f603c228259eb759bb
mitty1293 <k.fmichi@gmail.com>
2024-12-28 22:27:59 +0900

    「Reactを始める」完

--- mdn_web_docs/moz-todo-react/src/App.jsx
+++ mdn_web_docs/moz-todo-react/src/App.jsx
@@ -1,10 +1,11 @@
 import './App.css'
 
-function App() {
+function App(props) {
   return (
     <>
       <header>
-        <h1>Hello, World!</h1>
+        <h1>Hello, {props.subject}!</h1>
+        <button type='button' className='primary'>Click me!</button>
       </header>
     </>
   );
